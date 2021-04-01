cec1cb0eff12a92e75fd73898db1ad9ae8f9619a
Reed Essick <reed.essick@ligo.org>
2021-04-01 14:29:08 -0500

    added new files

---
+++ gwdetectors/cache/__init__.py
@@ -0,0 +1,8 @@
+"""a module storing known detector orientations and PSDs
+"""
+__author__ = "Reed Essick <reed.essick@gmail.com>"
+
+#-------------------------------------------------
+
+from .orientations import *
+from .psd import *
